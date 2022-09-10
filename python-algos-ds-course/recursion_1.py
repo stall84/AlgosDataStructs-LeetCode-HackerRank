68e9756092c0ffb2a9ca4a80ba125559f96f73c8
Michael Stallings <stall84@gmail.com>
2022-09-10 10:53:48 -0400

    Added a digit_sum recursion function

--- python-algos-ds-course/recursion_1.py
+++ python-algos-ds-course/recursion_1.py
@@ -17,11 +17,11 @@ def factorial(n):
         return n * factorial(n - 1)
 
 
-print(factorial(5))
+print("factorial algo running: ", factorial(5))
 
 
 # In the fibonaci algorithm below we're just asking what is the nTh (the input) element on the fibonaci sequence.
-def fibonaci(n):
+def fibonacci(n):
     # Step 1 - Validate input and edge-case safe
     assert n >= 0 and int(n) == n, 'Number must be a non-negative integer.'
     # Step 2 - Next set your base or exit condition. Unfortunately for us, a lot of these require understanding of fundamentally mathematical concepts like these 2 examples
@@ -29,7 +29,21 @@ def fibonaci(n):
         return n
     else:
         # Step 3 - Narrow your problem space - Do the actual recursion
-        return fibonaci(n-1) + fibonaci(n-2)
+        return fibonacci(n-1) + fibonacci(n-2)
 
 
-print(fibonaci(4))
+print("fibonacci algo running: ", fibonacci(4))
+
+
+def digit_sum(n):
+    assert n >= 0 and int(n) == n, 'Number must be a non-negative integer.'
+
+    # Our base condition should handle when we finally get down to 0. In this case, simply return and the call stack should collapse back down accordingly
+    if (n == 0):
+        return 0
+    else:
+        # We have to force n to be an integer to remove any decimals
+        return int(n % 10) + digit_sum(int(n // 10))
+
+
+print("digit_sum algo running: ", digit_sum(112))
