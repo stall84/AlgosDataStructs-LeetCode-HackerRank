ac17746b63f8c0184d0f160a94e8eab0213278aa
Michael Stallings <stall84@gmail.com>
2022-10-14 11:06:34 -0400

    Added a more optimized bubble sort function

--- js_sorting_algos/bubble_sort.ts
+++ js_sorting_algos/bubble_sort.ts
@@ -20,3 +20,21 @@ function my_bubble_sort(arr: number[]): number[] {
 }
 
 console.log(my_bubble_sort([4,1,9,8,5,3]))
+
+/**
+ * @description This implementation will save at least one loop by foregoing comparing the first element against itself on the first iteration
+ */
+function better_bubble_sort(arr: number[]): number[] {
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let swap = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = swap;
+            }
+        }
+    }
+    return arr;
+}
+
+console.log(better_bubble_sort([4,1,9,8,5,3]))
